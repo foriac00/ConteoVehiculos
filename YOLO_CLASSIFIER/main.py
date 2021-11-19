8e8bcb44eef517c117544dde98fbbca680c8e4a1
foriac00 <36696354+foriac00@users.noreply.github.com>
2021-11-19 13:46:04 +0100

    Add files via upload
    YOLO

---
+++ YOLO_CLASSIFIER/main.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+import sys
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = ["car"]
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Loading image
+img = cv2.imread("imagenes/test3.jpg")
+height, width, channels = img.shape
+
+# Detecting objects
+blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+# Showing informations on the screen
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            # Object detected
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            # Rectangle coordinates
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+print(indexes)
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        color = colors[class_ids[i]]
+        cv2.rectangle(img, (x, y), (x + w, y + h), color)
+        print 'Hay un coche en:','X', x, ',', 'Y', y
+
+print 'En la imagen hay', len(indexes), 'vehiculos'
+
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
